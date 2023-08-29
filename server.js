052ff05ce8b8e3705f17e676f7d0ca73ef6afe6a
Runia <runia5036@gmail.com>
2023-08-29 21:47:15 +0600

    third commit

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const http = require('http');
-//const url = require('url')
 const fs = require('fs');
 
 var server = http.createServer(function (req,res) {
 
-    if(req.url=='/file-write'){
+    if(req.url=='/'){
+
+        res.writeHead(200, {'Content-Type':'text/html'});
+        res.write("This is Home Page")
+        res.end();
+    }
+    else if(req.url=='/about'){
+
+        res.writeHead(200, {'Content-Type':'text/html'});
+        res.write("This is About Page")
+        res.end();
+    }
+    else if(req.url=='/contact'){
+
+        res.writeHead(200, {'Content-Type':'text/html'});
+        res.write("This is Contact Page")
+        res.end();
+    }
+    else if(req.url=='/file-write'){
 
         fs.writeFile('demo.txt','hello world',function (error){
 
@@ -25,9 +42,10 @@ var server = http.createServer(function (req,res) {
 
     }
 
+
 })
 
-server.listen(5100,()=>{
+server.listen(3000,()=>{
     console.log("Server create Successful");
 });
 
